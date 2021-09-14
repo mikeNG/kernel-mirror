304108a3f62efd092aafa8f841e876f4f8798623
Michael Bestas <mikeioannina@gmail.com>
2021-09-14 23:02:39 +0300

    Script to add git alternates to LineageOS kernels

---
+++ add-alternates.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+MIRROR_ROOT=${MIRROR_ROOT:=/mnt/mirrors}
+
+LINEAGE_MIRROR=${MIRROR_ROOT}/lineage
+MIRROR_MANIFEST=${MIRROR_ROOT}/mirror-manifest
+
+source ${MIRROR_MANIFEST}/kernel/metadata
+
+pushd ${LINEAGE_MIRROR}
+for repo in "${!kernel_map[@]}"; do
+	grep -q $repo ${MIRROR_MANIFEST}/default.xml
+	if [ $? -eq 0 ] ; then
+		if [ ! -d LineageOS/$repo.git ] ; then
+			echo "Creating empty repository:" LineageOS/$repo.git
+			mkdir -p LineageOS/$repo.git
+			GIT_DIR="LineageOS/$repo.git" git init --bare
+
+			case ${kernel_map[$repo]} in
+				2.6|3.0|3.1|3.4)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				3.10)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm-3.10.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				3.18)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm-3.18.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				4.4)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm-4.4.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				4.9)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm-4.9.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				4.14)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm-4.14.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				4.19)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm-4.19.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				5.4)
+					echo ${MIRROR_ROOT}/kernel/caf/kernel/msm-5.4.git/objects > LineageOS/$repo.git/objects/info/alternates
+					;;
+				*)
+					echo ${kernel_map[$repo]} "is not a valid kernel version"
+					;;
+			esac
+		fi
+	else
+		echo $repo.git "not found in mirror manifest!"
+	fi
+done
+popd
