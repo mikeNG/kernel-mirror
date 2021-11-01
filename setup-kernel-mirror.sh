bca6522d9bdbb91beb963b675400fd91ad62d274
Michael Bestas <mikeioannina@gmail.com>
2021-11-02 00:16:07 +0200

    setup-kernel-mirror: Check if directories exist

--- setup-kernel-mirror.sh
+++ setup-kernel-mirror.sh
@@ -12,50 +12,67 @@ AOSP_PROJECTS="kernel/arm64.git kernel/exynos.git kernel/goldfish.git kernel/gs.
 CAF_PROJECTS="kernel/msm.git kernel/msm-3.10.git kernel/msm-3.18.git kernel/msm-4.4.git kernel/msm-4.9.git kernel/msm-4.14.git kernel/msm-4.19.git kernel/msm-5.4.git"
 
 for mirror in ${MIRRORS}; do
-	mkdir -p ${MIRROR_ROOT}/${mirror}
+	if [ ! -d ${MIRROR_ROOT}/${mirror} ] ; then
+		mkdir -p ${MIRROR_ROOT}/${mirror}
+	fi
 done
 
 pushd ${MIRROR_ROOT}/linux
-repo init -u https://github.com/mikeNG/kernel-mirror --mirror
+if [ ! -d .repo ] ; then
+	repo init -u https://github.com/mikeNG/kernel-mirror --mirror
+fi
 
 repo sync torvalds/linux
 
-mkdir -p stable/linux.git
-GIT_DIR="stable/linux.git" git init --bare
-echo ${MIRROR_ROOT}/linux/torvalds/linux.git/objects > stable/linux.git/objects/info/alternates
+if [ ! -d stable/linux.git ] ; then
+	mkdir -p stable/linux.git
+	GIT_DIR="stable/linux.git" git init --bare
+	echo ${MIRROR_ROOT}/linux/torvalds/linux.git/objects > stable/linux.git/objects/info/alternates
+fi
 
 repo sync
 popd
 
 pushd ${MIRROR_ROOT}/aosp
-repo init -u https://github.com/mikeNG/kernel-mirror --mirror --manifest-name aosp.xml
+if [ ! -d .repo ] ; then
+	repo init -u https://github.com/mikeNG/kernel-mirror --mirror --manifest-name aosp.xml
+fi
 
-mkdir -p kernel/common.git
-GIT_DIR="kernel/common.git" git init --bare
-echo ${MIRROR_ROOT}/${TORVALDS_LINUX}/objects > kernel/common.git/objects/info/alternates
-echo ${MIRROR_ROOT}/${STABLE_LINUX}/objects >> kernel/common.git/objects/info/alternates
+if [ ! -d kernel/common.git ] ; then
+	mkdir -p kernel/common.git
+	GIT_DIR="kernel/common.git" git init --bare
+	echo ${MIRROR_ROOT}/${TORVALDS_LINUX}/objects > kernel/common.git/objects/info/alternates
+	echo ${MIRROR_ROOT}/${STABLE_LINUX}/objects >> kernel/common.git/objects/info/alternates
+fi
+
 repo sync kernel/common
 
 for aosp_project in ${AOSP_PROJECTS}; do
-	mkdir -p $aosp_project
-	GIT_DIR="$aosp_project" git init --bare
-	echo ${MIRROR_ROOT}/${TORVALDS_LINUX}/objects > $aosp_project/objects/info/alternates
-	echo ${MIRROR_ROOT}/${STABLE_LINUX}/objects >> $aosp_project/objects/info/alternates
-	echo ${MIRROR_ROOT}/${GOOGLE_COMMON}/objects >> $aosp_project/objects/info/alternates
+	if [ ! -d $aosp_project ] ; then
+		mkdir -p $aosp_project
+		GIT_DIR="$aosp_project" git init --bare
+		echo ${MIRROR_ROOT}/${TORVALDS_LINUX}/objects > $aosp_project/objects/info/alternates
+		echo ${MIRROR_ROOT}/${STABLE_LINUX}/objects >> $aosp_project/objects/info/alternates
+		echo ${MIRROR_ROOT}/${GOOGLE_COMMON}/objects >> $aosp_project/objects/info/alternates
+	fi
 done
 
 repo sync
 popd
 
 pushd ${MIRROR_ROOT}/caf
-repo init -u https://github.com/mikeNG/kernel-mirror --mirror --manifest-name caf.xml
+if [ ! -d .repo ] ; then
+	repo init -u https://github.com/mikeNG/kernel-mirror --mirror --manifest-name caf.xml
+fi
 
 for caf_project in ${CAF_PROJECTS}; do
-	mkdir -p $caf_project
-	GIT_DIR="$caf_project" git init --bare
-	echo ${MIRROR_ROOT}/${TORVALDS_LINUX}/objects > $caf_project/objects/info/alternates
-	echo ${MIRROR_ROOT}/${STABLE_LINUX}/objects >> $caf_project/objects/info/alternates
-	echo ${MIRROR_ROOT}/${GOOGLE_COMMON}/objects >> $caf_project/objects/info/alternates
+	if [ ! -d $caf_project ] ; then
+		mkdir -p $caf_project
+		GIT_DIR="$caf_project" git init --bare
+		echo ${MIRROR_ROOT}/${TORVALDS_LINUX}/objects > $caf_project/objects/info/alternates
+		echo ${MIRROR_ROOT}/${STABLE_LINUX}/objects >> $caf_project/objects/info/alternates
+		echo ${MIRROR_ROOT}/${GOOGLE_COMMON}/objects >> $caf_project/objects/info/alternates
+	fi
 done
 
 repo sync
