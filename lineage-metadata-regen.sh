19fd01f95d5b18aba5c8ba4f9e6ac5d03b288d03
Michael Bestas <mikeioannina@gmail.com>
2021-11-03 01:30:34 +0200

    Fix kernel metadata regeneration
    * Ignore some non kernel repos while we are at it

--- lineage-metadata-regen.sh
+++ lineage-metadata-regen.sh
@@ -6,9 +6,9 @@ LINEAGE_MIRROR=${MIRROR_ROOT}/lineage
 MIRROR_MANIFEST=${MIRROR_ROOT}/lineage-mirror-manifest
 KERNEL_MIRROR_MANIFEST=${MIRROR_ROOT}/kernel-mirror-manifest
 
-kernels=`grep kernel ${MIRROR_MANIFEST}/default.xml | sed -e 's#  <project name="LineageOS/##g' -e 's#" />##g'`
+kernels=`grep kernel ${MIRROR_MANIFEST}/default.xml | grep -v -e 'android_device' -e 'android_external' -e 'android_hardware' -e 'hardware_nvidia' | sed -e 's#  <project name="LineageOS/##g' -e 's#" />##g'`
 
-echo -e "declare -A kernel_map\n" > ${MIRROR_MANIFEST}/kernel/metadata
+echo -e "declare -A kernel_map\n" > ${KERNEL_MIRROR_MANIFEST}/lineage-metadata
 
 for kernel in $kernels; do
 	# set current VERSION, PATCHLEVEL
